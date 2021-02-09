975e01e50df2c37cc9b6ce066f2fb968eeda9cf9
Brahma Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2021-02-09 11:23:04 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@
 pacstrap /mnt base linux
 genfstab /mnt > /mnt/etc/fstab
 arch-chroot /mnt /bin/bash -c "yes "" | pacman -S nano sudo networkmanager grub gnome xorg-xinit pulseaudo gdm; echo '%sudo	ALL=(ALL) ALL' > /etc/sudoers; useradd arch; groupadd sudo; usermod -aG sudo arch; mkdir -p /home/arch; chown arch:arch /home/arch --recursive; echo 'arch:arch' | chpasswd; mkdir -p /boot/grub; grub-mkconfig -o /boot/grub/grub.cfg; systemctl enable NetworkManager"
-yes | pacman -Sy grub
+yes "" | pacman -Sy grub
 grub-install /disk --root-directory=/mnt
 echo "done! time to reboot!"
